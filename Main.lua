e2c945aa4803078a55ea2cf2d254cd1f6b18f948
uiems0 <alexshorty568@gmail.com>
2026-09-01 20:09:38 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,23 @@
-local function BubbleSort(LtG,SortingTable) --Ill do more here in like two days, i wanna finish my type spped thing tho
+function GetRotation(PivotPointX,PivotPointY,PointX,PointY,Degrees)
 
+    if not PointX or type(PointX) ~= "number" or not PointY or type(PointY) ~= "number" or not Degrees or type(Degrees) ~= "number" then
+        print("One of your variables are not valid!",PointX,PointY,Degrees)
+    end
+
+    Degrees = math.rad(Degrees)
+
+    if type(PivotPointX) ~= "number" then
+       PivotPointX = 0
+    end
+
+    if type(PivotPointY) ~= "number" then
+        PivotPointY = 0
+    end
+
+    local NewXCord = (PointX - PivotPointX)*math.cos(Degrees)-(PointY-PivotPointY)*math.sin(Degrees)+PivotPointX
+    local NewYCord =  (PointX - PivotPointX)*math.sin(Degrees)+(PointY-PivotPointY)*math.cos(Degrees)+PivotPointY
+
+    print("(" .. NewXCord .. "," .. NewYCord .. ")")
+
+    return NewXCord,NewXCord
 end
